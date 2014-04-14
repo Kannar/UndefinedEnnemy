da835d53a66b14f67d05970565cb12caf19e959e
Kannar <lucas.valentin.isart@gmail.com>
2014-04-14 15:46:20 +0200

    Ajout garbage et loader d'image

---
+++ scripts/js/config/image_data.js
@@ -0,0 +1,14 @@
+/********************************************
+*   Tableau des chemins des sources         *
+********************************************/
+var imgSrc = {
+    // "guerrier" : "medias/graphic/guerrier1.png"
+};
+
+/********************************************
+*   Tableau des objets images (rempli une   *
+*   fois le loading terminé                 *
+********************************************/
+var imgSObj = {
+    // "guerrier" : objet JS image
+};
