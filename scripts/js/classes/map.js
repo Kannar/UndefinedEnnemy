9a71605c053c178af0e8c1d977c091e460b40afc
Kannar <lucas.valentin.isart@gmail.com>
2014-04-14 16:21:46 +0200

    Correction map.js

--- scripts/js/classes/map.js
+++ scripts/js/classes/map.js
@@ -1,27 +1,19 @@
-var Map = Class.extend({
- init:function(map){
-  this.map=map;
-  // this.buildmap=buildmap;
- },
- buildmap:function(map)
- {
-  
- }
-});
-var MapVolcanique = Map.extend({
- buildmap:function(){
-  for(var i =0; i < this.map.length;i++)
-  {
-   for(var j =0; j < this.map[i].length;j++)
-   {
-    if(this.map[i][j]==1)
-     context.fillStyle="rgb(0,255,140)";
-    else if(this.map[i][j]==3)
-     context.fillStyle="rgb(255,0,140)";
-    else if(this.map[i][j]==0 || this.map[i][j]==4)
+var Map = function(matrice){
+ this.matrice = matrice;
+
+ this.constructMap=function(){
+  for (var i = 0; i < matrice.length; i++) {
+   for (var j = 0; j < matrice[i].length; j++) {
+    if(matrice[i][j]==1)
+    {
+     context.fillStyle="rgb(255,0,0)";
+    }
+    else
+    {
      context.fillStyle="rgb(255,255,255)";
+    }
     context.fillRect(0+j*66,0+i*66,65,65);
-   }
-  }
+   };
+  };
  }
-});
+}
