7bcabfd86b1e77b643b67bfdd35f5bb3bac6ea06
Federico <federico.giacomini@ymail.com>
2014-04-16 16:40:11 +0200

    Commit range + player

--- scripts/js/config/image_data.js
+++ scripts/js/config/image_data.js
@@ -6,6 +6,15 @@ var imgSrc = {
 	"ArcherPlayer2" : "images/Heros/Archer2.png",
 	"ThiefPlayer1" : "images/Heros/Thief1.png",
 	"ThiefPlayer2" : "images/Heros/Thief2.png",
+	"DragonPlayer1" : "images/Heros/Dragon1.png",
+	"DragonPlayer2" : "images/Heros/Dragon2.png",
+	"KnightPlayer1" : "images/Heros/Knight1.png",
+	"KnightPlayer2" : "images/Heros/Knight2.png",
+	"PriestPlayer1" : "images/Heros/Priest1.png",
+	"PriestPlayer2" : "images/Heros/Priest2.png",
+	"MagePlayer1" : "images/Heros/Mage1.png",
+	"MagePlayer2" : "images/Heros/Mage2.png",
+
 };
 
 /********************************************
--- scripts/js/eventInit.js
+++ scripts/js/eventInit.js
@@ -43,7 +43,7 @@ function endPathFinding(e){
 function resetClick(e){
     //if(mouse.findCase(e.clientX, e.clientY).x ||)
 }
-function showCharRange(case1,range){
+function showCharRange(case1,range,rangeattack){
     var checkPath;
     for(var i=-range;i<=range;i++)
     {
@@ -63,6 +63,29 @@ function showCharRange(case1,range){
                     if(checkPath.length<=range+1 && map1["collisions"][j+case1.y][i+case1.x]!=1)
                     {
                      context.fillStyle="rgba(0,0,255,0.5)";
+                     context.fillRect((i+(case1.x-mapParams.viewX))*mapParams.tileSize,(j+case1.y-mapParams.viewY)*mapParams.tileSize,65,65);
+                    }
+                }
+            
+            }
+        }
+        rangeattack = 3
+        for(var j=-(rangeattack);j<=rangeattack;j++)
+        {
+            if(Math.abs(i)+Math.abs(j)<=rangeattack)
+            {
+                if(j+case1.y>=0 && j+case1.y<mapParams.nbCaseMapY)
+                {
+                   // console.log(i+case1.x,j+case1.y)
+                    var grid = new PF.Grid(mapParams.nbCaseMapY, mapParams.nbCaseMapX, map1["collisions"]);
+                    grid.setWalkableAt(0, 1, false);
+                    var finder = new PF.AStarFinder();
+                    var gridBackup = grid.clone();
+                    checkPath = finder.findPath(case1.x, case1.y, i+case1.x,j+case1.y , grid);
+                    grid = gridBackup;
+                    if(checkPath.length<=rangeattack+1 && map1["collisions"][j+case1.y][i+case1.x]!=1)
+                    {
+                     context.fillStyle="rgba(255,0,0,0.5)";
                      context.fillRect((i+(case1.x-mapParams.viewX))*mapParams.tileSize,(j+case1.y-mapParams.viewY)*mapParams.tileSize,65,65);
                     }
                 }
