a36755587c9e5032d2682cc5c68d5fbb6791fd18
Federico <federico.giacomini@ymail.com>
2014-04-16 17:10:13 +0200

    upup

--- scripts/js/config/image_data.js
+++ scripts/js/config/image_data.js
@@ -2,10 +2,19 @@
 *   Tableau des chemins des sources         *
 ********************************************/
 var imgSrc = {
-	"ArcherPlayer1" : "images/Heros/Archer1.png",
-	"ArcherPlayer2" : "images/Heros/Archer2.png",
+	//"ArcherPlayer1" : "images/Heros/Archer1.png",
+	//"ArcherPlayer2" : "images/Heros/Archer2.png",
 	"ThiefPlayer1" : "images/Heros/Thief1.png",
 	"ThiefPlayer2" : "images/Heros/Thief2.png",
+	"DragonPlayer1" : "images/Heros/Dragon1.png",
+	"DragonPlayer2" : "images/Heros/Dragon2.png",
+	"KnightPlayer1" : "images/Heros/Knight1.png",
+	"KnightPlayer2" : "images/Heros/Knight2.png",
+	"MagePlayer1" : "images/Heros/Mage1.png",
+	"MagePlayer2" : "images/Heros/Mage2.png",
+	"PriestPlayer1" : "images/Heros/Priest1.png",
+	"PriestPlayer2" : "images/Heros/Priest2.png",
+
 };
 
 /********************************************
