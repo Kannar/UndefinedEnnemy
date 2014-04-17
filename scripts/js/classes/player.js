7f0294f816d502ae5d888f91cc1877260bc001e9
Federico <federico.giacomini@ymail.com>
2014-04-17 13:39:10 +0200

    upup

--- scripts/js/classes/player.js
+++ scripts/js/classes/player.js
@@ -71,12 +71,15 @@ Player.prototype.onclick = function(x,y){
 			}
 		} 
 		else if(this.army[i].isSelected) {
-			this.army[i].move();
-		}
-		if(this.army[i].isSelected)
-		{
-			if(this.army[i].checkEnnemiInRange()){
-				this.army[i].chooseTarget(caseSelected);
+			if(this.army[i].CheckCase(caseSelected) == 'move'){
+				this.army[i].move();
+			}
+			if(this.army[i].CheckCase(caseSelected) == 'player'){
+				var enemy = this.army[i].checkEnnemiInRange(caseSelected);
+				if(enemy){
+					this.army[i].attack(enemy);
+					this.army[i].targetAvaible = [];
+				}
 			}
 		}
 	};
